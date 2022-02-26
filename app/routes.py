9601c1e65f198dc0d7eef102fde50f1394be8290
John-Kimani <kimwaweru100@gmail.com>
2022-02-26 12:55:19 +0300

    Dev: Create home template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Welcome to Horizon News'
+    return render_template('index.html')
