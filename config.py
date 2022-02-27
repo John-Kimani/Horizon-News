4cb9ea8b67902842869a4289c7b09442c663811d
John-Kimani <kimwaweru100@gmail.com>
2022-02-27 21:36:49 +0300

    Fix: Key error

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ class Config(object):
     Parent class configuration for horizon news web app
     '''
     NEWS_SOURCE_URL = os.environ.get('NEWS_SOURCE_URL')
+    #test
+    # NEWS_SOURCE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
     NEWS_ARTICLE_API_URL = os.environ.get('NEWS_ARTICLE_API_URL')
     NEWS_APIKEY = os.environ.get('NEWS_APIKEY')
