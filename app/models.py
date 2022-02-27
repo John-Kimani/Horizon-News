7f536cbfd076a67714cd20464085c3221c7f1a26
John-Kimani <kimwaweru100@gmail.com>
2022-02-27 18:41:31 +0300

    Dev: creates source class with properties

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Sources:
+    '''
+    Class that defines news sources object
+    '''
+
+
+    def __init__(self, id, name, description, url, category, country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
