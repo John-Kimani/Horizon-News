ea3bedcb230641503043b1a0f13f67f107a84b02
John-Kimani <kimwaweru100@gmail.com>
2022-02-27 19:50:32 +0300

    Dev: Creates view function that displays news sources

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from app import app
+from .requests import get_source
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    '''
+    View function that returns index page and data
+    '''
+    news_sources = get_source('general')
+    print(news_sources)
+    return render_template('index.html', news_sources-news_sources)
