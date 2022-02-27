f9dfd4100e30167dab79dbfc3f4f7112f6cbd1c6
John-Kimani <kimwaweru100@gmail.com>
2022-02-27 21:00:31 +0300

    Dev: Create article class

--- app/models.py
+++ app/models.py
@@ -11,3 +11,21 @@ class Source:
         self.url = url
         self.category = category
         self.country = country
+
+
+# top headlines
+
+class Article:
+    '''
+    Class that defines article objects
+    '''
+
+    def __init__(self, source, author, title, description, url, urlToImage, publishedAt, content):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImahe = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
