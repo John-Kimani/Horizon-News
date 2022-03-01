452ad50266e07bdaef776cbf1250739f3c3b0ea3
John-Kimani <kimwaweru100@gmail.com>
2022-03-01 12:15:07 +0300

    Fix: NewsAPI url

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ class Config(object):
     '''
     Parent class configuration for horizon news web app
     '''
-    NEWS_SOURCE_URL = os.environ.get('NEWS_SOURCE_URL')
+    # NEWS_SOURCE_URL = os.environ.get('NEWS_SOURCE_URL')
     #test
-    # NEWS_SOURCE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
-    NEWS_ARTICLE_URL = os.environ.get('NEWS_ARTICLE_URL')
-    NEWS_TOP_STORIES_API_URL = os.environ.get('NEWS_TOP_STORIES_API_URL')
+    NEWS_SOURCE_URL='https://newsapi.org/v2/sources?category={}&apiKey={}'
+    NEWS_ARTICLE_URL='https://newsapi.org/v2/everything?q={}&sortBy=popularity&apiKey={}'
+    # NEWS_ARTICLE_URL = os.environ.get('NEWS_ARTICLE_URL')
+    # NEWS_TOP_STORIES_API_URL = os.environ.get('NEWS_TOP_STORIES_API_URL')
     NEWS_APIKEY = os.environ.get('NEWS_APIKEY')
 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
